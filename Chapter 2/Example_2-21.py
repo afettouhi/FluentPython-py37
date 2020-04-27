2a7b1d880e9872de8f68b20c66970ac168c5bc6e
André Fettouhi <A.Fettouhi@gmail.com>
2020-04-27 09:39:30 +0200

    Fixed Example_2-21.py to work with latest array module.

--- Chapter 2/Example_2-21.py
+++ Chapter 2/Example_2-21.py
@@ -1,6 +1,6 @@
 from array import array
 
-numbers = array.array('h', [-2, -1, 0, 1, 2])
+numbers = array('h', [-2, -1, 0, 1, 2])
 memv = memoryview(numbers)
 len(memv)
 
@@ -8,6 +8,5 @@ memv[0]
 
 memv_oct = memv.cast('B')
 memv_oct.tolist()
-[254, 255, 255, 255, 0, 0, 1, 0, 2, 0]
 memv_oct[5] = 4
 numbers
